1f49a01fcab9a051d2ba78c8cdd061b0f7a8ec50
wagonrepairer@gmail.com <wagonrepairer@gmail.com@localhost>
2013-04-30 15:22:35 +0000

    Don't focus on chat input if there is selected text.
    svn path=/trunk/; revision=317

--- site/lwidgets/Chat.js
+++ site/lwidgets/Chat.js
@@ -130,8 +130,32 @@ define(
 	{
 	},
 	
+	getSelectedText:function()
+	{
+		var html = "";
+		if (typeof window.getSelection != "undefined") {
+			var sel = window.getSelection();
+			if (sel.rangeCount) {
+				var container = document.createElement("div");
+				for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+					container.appendChild(sel.getRangeAt(i).cloneContents());
+				}
+				html = container.innerHTML;
+			}
+		} else if (typeof document.selection != "undefined") {
+			if (document.selection.type == "Text") {
+				html = document.selection.createRange().htmlText;
+			}
+		}
+		return html;
+	},
+	
 	'focusTextNode':function(e)
 	{
+		if( this.getSelectedText() !== '' )
+		{
+			return;
+		}
 		this.textInputNode.focus();
 	},
 	
