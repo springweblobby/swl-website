f279605f63b1fc6ca4bb3884cd335ac30fdb8670
ikinz <danny.b.eml@gmail.com>
2014-09-29 16:27:23 +1100

    Rename ConnectButton.js to jsx.

--- weblobby.jsx
+++ weblobby.jsx
@@ -2,7 +2,7 @@
 (function(){
     'use strict'
 
-	var ConnectButton = require('./comp/ConnectButton.js');
+	var ConnectButton = require('./comp/ConnectButton.jsx');
 
 	React.renderComponent(<ConnectButton />, document.getElementById('main'));
 })()
