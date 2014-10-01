cb187ff8225b64143e6d7c357a712187f8e2af38
ikinz <danny.b.eml@gmail.com>
2014-10-02 00:11:41 +1100

    Show lobby and os icons in user list.

--- comp/UserItem.jsx
+++ comp/UserItem.jsx
@@ -2,9 +2,41 @@
 
 'use strict'
 
+var _ = require('lodash');
+
 module.exports = React.createClass({
 	render: function(){
-		var user = this.props.user;
-		return <li className="userItem"><img src={'img/flags/' + user.country.toLowerCase() + '.png'} /> {user.name}</li>
+		var user = {
+			country: 'unknown',
+			cpu: 0,
+		};
+		_.extend(user, this.props.user);
+		var pics = [];
+
+		// Lobby
+		if ( _.indexOf( ['7777', '7778', '7779'], user.cpu ) !== -1 )
+			pics.push(<img src = "img/blobby2icon-small.png" title = "Spring Web Lobby" />);
+		else if ( _.indexOf( ['6666', '6667', '6668'], user.cpu ) !== -1 )
+			pics.push(<img src = "img/zk_logo_square.png" title = "Zero-K Lobby" />);
+		else if ( _.indexOf( ['9997', '9998', '9999'], user.cpu ) !== -1 )
+			pics.push(<img src = "img/notalobby.png" title = "NotaLobby" />);
+		else if ( _.indexOf( ['8484'], user.cpu ) !== -1 )
+			pics.push(<img src = "img/mlclient.ico" title = "mlclient" />);
+		else if ( _.indexOf( ['4607052', '4607063', '4607053'], user.cpu ) !== -1 )
+			pics.push(<img src = "img/flobby.png" title = "flobby" />);
+
+		// OS
+		if ( _.indexOf([ '7777', '9998', '6667', '4607063' ], user.cpu) !== -1 )
+			pics.push(<img src="img/windows.png" title = "Microsoft Windows" />);
+		else if ( _.indexOf([ '7778', '9999', '6668', '4607052' ], user.cpu) !== -1 )
+			pics.push(<img src="img/linux.png" title = "Linux" />);
+		else if ( _.indexOf([ '7779', '9997', '4607053' ], user.cpu) !== -1 )
+			pics.push(<img src="img/mac.png" title = "MacOS" />);
+
+		return (<li className="userItem">
+				<img className="userFlag" src={'img/flags/' + user.country.toLowerCase() + '.png'} />
+				{user.name}
+				<div className="userPics">{pics}</div>
+			</li>);
 	}
 });
