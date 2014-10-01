44b06f68349cdaaee417d6f4250d0c9a59be9a3c
ikinz <danny.b.eml@gmail.com>
2014-10-02 00:54:00 +1100

    Show number of users in user list header.

--- comp/UserList.jsx
+++ comp/UserList.jsx
@@ -31,21 +31,22 @@ module.exports = React.createClass({
 	},
 	render: function(){
 		var users = this.state.users;
+		var userItems = _.keys(users).filter(function(x){
+			return !this.state.filtering || !!(x.match(this.state.filter));
+		}.bind(this)).sort().map(function(x){
+			return <UserItem key={users[x].name} user={users[x]} />;
+		});
 		return (<ul className="userList">
 			<li className="listHeader">
 				<span style={{ display: this.state.filtering ? 'none' : 'inline' }} onClick={this.showFilter}>
-					Users <span className="listTip">(click to filter)</span>
+					{userItems.length} users <span className="listTip">(click to filter)</span>
 				</span>
 				<span style={{ display: this.state.filtering ? 'inline' : 'none' }}>
 					<input className="listFilter" type="text" ref="filterInput" valueLink={this.linkState('filter')} />
 					<a className="filterHide" herf="#" onClick={this.hideFilter}>×</a>
 				</span>
 			</li>
-			{_.keys(users).filter(function(x){
-				return !this.state.filtering || !!(x.match(this.state.filter));
-			}.bind(this)).sort().map(function(x){
-				return <UserItem key={users[x].name} user={users[x]} />;
-			})}
+			{userItems}
 		</ul>);
 	}
 });
