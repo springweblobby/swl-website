eb5659314b7c31e7fa695dba479072a5d962e28b
ikinz <danny.b.eml@gmail.com>
2014-10-12 22:25:49 +1100

    Preliminary rendering for BattleUserList.

--- comp/BattleUserList.jsx
+++ comp/BattleUserList.jsx
@@ -9,12 +9,23 @@ var _ = require('lodash');
 var UserItem = require('./UserItem.jsx');
 
 module.exports = React.createClass({
+	handleTeamClick: function(n){
+		this.props.onChangeTeam(n);
+	},
 	render: function(){
-		var allUsers = _.flatten(_.map(this.props.teams, _.partialRight(_.map, _.identity)));
+		var userCount = _.reduce(_.map(this.props.teams, _.size), function(a, b){ return a + b }, 0);
 		return (<div className="userList">
 			<div className="listHeader">
-				{allUsers.length - _.size(this.props.teams[0])} players, {_.size(this.props.teams[0])} spectators
+				{userCount - _.size(this.props.teams[0])} players, {_.size(this.props.teams[0])} spectators
 			</div>
+			<ul>
+			{_.map(this.props.teams, function(team, num){
+				return [<li className="listTeam" onClick={this.handleTeamClick} key={'t'+num}>Team {num}</li>].
+					concat(_.map(team, function(user){
+						return <UserItem user={user} />
+				}.bind(this)));
+			}.bind(this))}
+			</ul>
 		</div>);
 	}
 });
