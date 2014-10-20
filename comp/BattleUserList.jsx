0b8e1445ab372af70b733a571c93a6485fe88782
ikinz <danny.b.eml@gmail.com>
2014-10-21 02:20:25 +1100

    Always show specs and team 1/2 in battle user list

--- comp/BattleUserList.jsx
+++ comp/BattleUserList.jsx
@@ -12,8 +12,19 @@ module.exports = React.createClass({
 	handleTeamClick: function(n){
 		this.props.onChangeTeam(n);
 	},
+	renderTeamHeader: function(num){
+		return (<li className="listTeam" key={'t'+num}>
+			<span className="listTeamName">{num == 0 ? 'Spectators' : 'Team ' + num}</span>
+			<span className="listTeamButtons">
+				<button onClick={_.partial(this.handleTeamClick, num)}>join</button>
+				<button>add bot</button>
+			</span>
+		</li>);
+	},
 	render: function(){
 		var userCount = _.reduce(_.map(this.props.teams, _.size), function(a, b){ return a + b }, 0);
+		// Always show Spectators, Team 1 and Team 2.
+		var teams = _.defaults(this.props.teams, { 0: {}, 1: {}, 2: {} });
 		return (<div className="userList">
 			<div className="listHeader">
 				{userCount - _.size(this.props.teams[0])} players, {_.size(this.props.teams[0])} spectators
@@ -24,17 +35,20 @@ module.exports = React.createClass({
 				</span>
 			</div>
 			<ul>
-			{_.map(this.props.teams, function(team, num){
-				return [<li className="listTeam" onClick={_.partial(this.handleTeamClick, num)} key={'t'+num}>
-					<span className="listTeamName">Team {num}</span>
-					<span className="listTeamButtons">
-						<button>join</button>
-						<button>add bot</button>
-					</span>
-				</li>].concat(_.map(team, function(user){
-					return <UserItem user={user} />
+
+			{/* Teams */}
+			{_.map(_.omit(teams, '0'), function(team, num){
+				return [this.renderTeamHeader(num)].concat(_.map(team, function(user){
+					return <UserItem user={user} key={user.name} />
 				}.bind(this)));
 			}.bind(this))}
+
+			{/* Spectators */}
+			{this.renderTeamHeader(0)}
+			{_.map(teams[0], function(user){
+				return <UserItem user={user} key={user.name} />
+			}.bind(this))}
+
 			</ul>
 		</div>);
 	}
