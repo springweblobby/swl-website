a7a67cbbfd68fcbccb7a768273b21958b559c028
ikinz <danny.b.eml@gmail.com>
2014-10-21 21:36:29 +1100

    Show bot count in battle user list header.

--- comp/BattleUserList.jsx
+++ comp/BattleUserList.jsx
@@ -21,17 +21,23 @@ module.exports = React.createClass({
 			<span className="listTeamName">{num == 0 ? 'Spectators' : 'Team ' + num}</span>
 			<span className="listTeamButtons">
 				<button onClick={_.partial(this.props.onChangeTeam, num)}>join</button>
-				<button onClick={_.partial(this.props.onAddBot, num)}>add bot</button>
+				{num == 0 ? null : <button onClick={_.partial(this.props.onAddBot, num)}>add bot</button>}
 			</span>
 		</li>);
 	},
 	render: function(){
-		var userCount = _.reduce(_.map(this.props.teams, _.size), function(a, b){ return a + b }, 0);
+		// Sum of all elements of a collection.
+		var sum = _.partialRight(_.reduce, function(a, b){ return a + b; }, 0);
+		var userCount = sum(_.map(this.props.teams, _.size));
+		// Ignores bots that are spectators.
+		var botCount = sum(_.map(_.omit(this.props.teams, '0'),
+			function(t){ return _.size(_.filter(t, { bot: true })); }));
 		// Always show Spectators, Team 1 and Team 2.
 		var teams = _.defaults(this.props.teams, { 0: {}, 1: {}, 2: {} });
 		return (<div className="userList">
 			<div className="listHeader">
-				{userCount - _.size(this.props.teams[0])} players, {_.size(this.props.teams[0])} spectators
+				{userCount - _.size(this.props.teams[0]) - botCount} players,
+				{' ' + botCount} bots, {_.size(this.props.teams[0])} spectators
 				<span className="listHeaderButtons">
 					<button>balance</button>
 					<button>spec afk</button>
