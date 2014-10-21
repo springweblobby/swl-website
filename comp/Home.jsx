d366ac87872c131c14397563180ce5a99f403422
ikinz <danny.b.eml@gmail.com>
2014-10-21 23:46:57 +1100

    Add skirmish buttons for Evo.

--- comp/Home.jsx
+++ comp/Home.jsx
@@ -7,15 +7,29 @@
 
 var _ = require('lodash');
 var Screens = require('./ScreenTypes.js');
+var ModalWindow = require('./ModalWindow.jsx');
 var Battle = require('../act/Battle.js');
 
 module.exports = React.createClass({
-	handleEvoSkirmish: function(){
-		Battle.openSinglePlayerBattle('Evolution RTS Skirmish', function(){
+	getInitialState: function(){
+		return {
+			addingEvoSpawner: false,
+		};
+	},
+	handleEvoSkirmish: function(bot){
+		Battle.openSinglePlayerBattle('Skirmish vs ' + bot, function(){
 			this.setEngine('96.0');
 			this.setGame('Evolution RTS - v8.04');
 			this.setMap('OnyxCauldron1.6');
+			this.addBot(bot, 'Enemy', 2);
 		});
+		this.setState({ addingEvoSpawner: false });
+	},
+	handleEvoSpawner: function(){
+		this.setState({ addingEvoSpawner: true });
+	},
+	handleEvoCancel: function(){
+		this.setState({ addingEvoSpawner: false });
 	},
 	handleCustomSkirmish: function(){
 		Battle.openSinglePlayerBattle('Custom Skirmish', _.noop);
@@ -23,9 +37,21 @@ module.exports = React.createClass({
 	render: function(){
 		return (<div className="homeScreen">
 			<button>Multiplayer</button>
-			<button onClick={this.handleEvoSkirmish}>Evolution RTS Skirmish</button>
+			<button onClick={_.partial(this.handleEvoSkirmish, 'Shard')}>Skirmish vs Shard</button>
+			<button onClick={this.handleEvoSpawner}>Skirmish vs Survival Spawner</button>
 			<button onClick={this.handleCustomSkirmish}>Custom Skirmish</button>
 			<button onClick={_.partial(this.props.onSelect, Screens.SETTINGS)}>Settings</button>
+
+			{this.state.addingEvoSpawner ?
+				<ModalWindow onClose={this.handleEvoCancel} title="Choose difficulty">
+					{['Very Easy', 'Easy', 'Normal', 'Hard', 'Very Hard'].map(function(diff){
+						return (<button
+							onClick={_.partial(this.handleEvoSkirmish, 'Survival Spawner: ' + diff)}>
+							{diff}
+						</button>)
+					}.bind(this))}
+				</ModalWindow>
+			: null}
 		</div>);
 	}
 });
