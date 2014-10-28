bd51d4a8e31a6a7c136c8e81fee61c4f9772e6dd
ikinz <danny.b.eml@gmail.com>
2014-10-28 14:08:46 +1000

    Add release mode tasks for grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,13 @@ module.exports = function(grunt){
 					browserifyOptions: { debug: true }
 				}
 			},
+			release: {
+				src: 'weblobby.jsx',
+				dest: 'weblobby.bundle.js',
+				options: {
+					transform: ['reactify', 'uglifyify'],
+				}
+			},
 		},
 		sass: {
 			debug: {
@@ -19,6 +26,19 @@ module.exports = function(grunt){
 					dest: 'css/',
 					ext: '.css'
 				}],
+			},
+			release: {
+				files: [{
+					expand: true,
+					cwd: 'sass/',
+					src: ['**/*.sass', '**/*.scss'],
+					dest: 'css/',
+					ext: '.css'
+				}],
+				options: {
+					style: 'compressed',
+					sourcemap: 'none'
+				}
 			},
 		},
 		watch: {
@@ -31,9 +51,26 @@ module.exports = function(grunt){
 				tasks: ['sass:debug']
 			},
 		},
+		uglify: {
+			uglify: {
+				files: {
+					'weblobby.bundle.js': ['weblobby.bundle.js']
+				}
+			}
+		},
+	});
+
+	grunt.registerTask('release', ['browserify:release', 'sass:release', 'uglify']);
+	grunt.registerTask('debug', ['browserify:debug', 'sass:debug']);
+	grunt.registerTask('default', 'Help', function(){
+		grunt.log.write("\n\nUSAGE\n\n" +
+			"Use 'grunt watch' to have grunt rebuild the poject in debug mode whenever something changes.\n" +
+			"You can also use 'grunt debug' to build in debug mode explicitly.\n" +
+			"Run 'grunt release' to build in release mode.\n\n");
 	});
 
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 };
