115a06c5b1d3260018182b5a7128e4083ea02fa9
ikinz <danny.b.eml@gmail.com>
2014-10-31 00:18:27 +1000

    Drop the Manager suffix from Screen/ChatManager.
    It's misleading as neither of them actually manage much.

--- weblobby.jsx
+++ weblobby.jsx
@@ -4,11 +4,11 @@
 
 window.QWeblobbyApplet && QWeblobbyApplet.init();
 
-var ScreenManager = require('./comp/ScreenManager.jsx');
+var AppScreens = require('./comp/AppScreens.jsx');
 
 var App = React.createClass({
 	render: function(){
-		return <ScreenManager />;
+		return <AppScreens />;
 	}
 });
 
