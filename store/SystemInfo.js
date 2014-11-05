23a4fecbaaa4590876f57a31a882f6f68d60a2e8
ikinz <danny.b.eml@gmail.com>
2014-11-06 00:43:01 +1000

    Add SystemInfo.

---
+++ store/SystemInfo.js
@@ -0,0 +1,21 @@
+/*
+ * Provides some system info.
+ */
+
+var Applet = require('./Applet.js');
+
+module.exports = {
+	platform: (function(){
+		if (navigator.platform.match(/Win/))
+			return "Windows";
+		else if (navigator.platform.match(/Mac/))
+			return "Mac";
+		else if (navigator.platform.match(/Linux.*x86_64/))
+			return "Linux64";
+		else if (navigator.platform.match(/Linux/))
+			return "Linux";
+		else
+			return "";
+	})(),
+	springHome: Applet ? Applet.getSpringHome() : "springHome",
+};
