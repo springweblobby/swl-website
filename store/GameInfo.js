b4e0cf99527632faf7c1ddf91297c00140f23f6d
ikinz <danny.b.eml@gmail.com>
2014-11-13 20:30:52 +1000

    Prevent erroneous dead code path removal.

--- store/GameInfo.js
+++ store/GameInfo.js
@@ -1,2 +1,3 @@
 // Route to the mock implementation if we're running in browser.
+require('./GameInfoMock.js'); // prevents the minifier from removing it
 module.exports = require('./Applet.js') ? require('./GameInfoImpl.js') : require('./GameInfoMock.js');
