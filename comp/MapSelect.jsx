3c4f5ea2fd0689638ea4914db08181c3a23f7ff8
ikinz <danny.b.eml@gmail.com>
2014-12-26 03:45:07 +1000

    Add filtering and highlight maps on hover.

--- comp/MapSelect.jsx
+++ comp/MapSelect.jsx
@@ -6,19 +6,30 @@
 
 var _ = require('lodash');
 
+// This is based on the sroll size used by zk site.
+// See https://github.com/ZeroK-RTS/Zero-K-Infrastructure/blob/master/Zero-K.info/AppCode/Global.cs#L41
+var scrollSize = 40;
+
 module.exports = React.createClass({
+	mixins: [React.addons.LinkedStateMixin],
 	getInitialState: function(){
-		return { entriesShowing: 32 };
+		return {
+			filter: '',
+			entriesShowing: scrollSize,
+		};
 	},
 	handleScroll: function(evt){
 		var node = evt.target;
 		if (node.scrollTop > node.scrollHeight - node.clientHeight * 1.5)
-			this.setState({ entriesShowing: this.state.entriesShowing + 32 });
+			this.setState({ entriesShowing: this.state.entriesShowing + scrollSize });
 	},
 	render: function(){
 		return (<div className="mapSelect">
+			<div>Filter: <input type="text" valueLink={this.linkState('filter')} /></div>
 			<div className="mapList" onScroll={this.handleScroll}>
-				{_.map(this.props.maps, function(val, key){
+				{_.map(_.pick(this.props.maps, function(val, key){
+					return key.match(new RegExp(this.state.filter, 'i'));
+				}.bind(this)), function(val, key){
 					return (<div className="mapIcon" key={key}
 								onClick={_.partial(this.props.onSelectMap, key)}>
 						<div className="thumb"><img src={val.thumbnail} /></div>
