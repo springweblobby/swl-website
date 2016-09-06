03c7683be3eed0a6054965db1db4ac6255b0d7eb
ikinz <danny.b.eml@gmail.com>
2016-09-07 09:37:23 +1000

    Show connected user label separately.
    Use the name you used for connecting instead of the one in settings.

--- comp/ConnectButton.jsx
+++ comp/ConnectButton.jsx
@@ -10,9 +10,10 @@ var LobbyServer = require('store/LobbyServer.js');
 
 module.exports = React.createClass({
 	displayName: 'ConnectButton',
-	mixins: [SPM.connect('serverStore', '', ['connection', 'users'])],
+	mixins: [SPM.connect('serverStore', '', ['connection', 'users', 'nick'])],
 	render: function(){
 		var onclick, img, label;
+		var userLabel = null;
 		if (this.state.connection === ConnectionState.DISCONNECTED) {
 			onclick = Server.connect;
 			img = require('img/red.png');
@@ -24,23 +25,30 @@ module.exports = React.createClass({
 		} else if (this.state.connection === ConnectionState.CONNECTED) {
 			onclick = Server.disconnect;
 			img = require('img/green.png');
-			label = Settings.name;
-			var user = this.state.users[Settings.name];
+			label = 'Connected';
+
+			var user = this.state.users[this.state.nick];
+			var userImg = "";
 			if (user.elo > 0 && user.level >= 0){ //zkls
 				var level = Math.max(1, Math.min(9, Math.floor(10 - 9 * Math.exp(-user.level/60))));
 				var skill = Math.max(0, Math.min(5, Math.floor((user.elo - 1200) / 200)));
-				img = require('img/ranks/' + level + '_' + skill + '.png');
+				userImg = require('img/ranks/' + level + '_' + skill + '.png');
 			}
 			else if (user.timeRank >= 0) { //spring
 				var level = user.timeRank + 1;
 				var skill = 2;
-				img = require('img/ranks/' + level + '_' + skill + '.png');
+				userImg = require('img/ranks/' + level + '_' + skill + '.png');
 			}
+			userLabel = <div><img src={userImg} /> {this.state.nick}</div>
 		} else { 
 			onclick = null;
 			img = null;
 			label = "[Unknown state, add support]";
 		}
-		return <button onClick={onclick}><img src={img} /> {label}</button>;
+		// Should probably move the user label into a separate component.
+		// Because now if you click on it you get disconnected.
+		return <button onClick={onclick}><img src={img} /> {label}
+			<div className="userLabel">{userLabel}</div>
+		</button>;
 	}
 });
